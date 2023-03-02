37f2f8f4fff59ab3cca636284becbb375ba1bd13
Longcat2957 <schrodingers030@gmail.com>
2023-03-02 14:52:13 +0900

    123

--- sisr.py
+++ sisr.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 import numpy as np
 import onnx
@@ -5,8 +6,14 @@ import onnxruntime as ort
 
 from data_utils import load_image, preprocess, postprocess
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--mode', type=str, default='int8', choices=['int8', 'fp32']
+)
+
 if __name__ == '__main__':
-    session = ort.InferenceSession('RLFN_fp32.onnx', providers=['CPUExecutionProvider'])
+    opt = parser.parse_args()
+    session = ort.InferenceSession(f'RLFN_{opt.mode}.onnx', providers=['CPUExecutionProvider'])
     input_name = session.get_inputs()[0].name
     
     orig_img = load_image('ms3_01.png')
@@ -19,6 +26,6 @@ if __name__ == '__main__':
     pp_result = postprocess(raw_result)
     
     import cv2
-    cv2.imwrite('ort_result_fp32.png', pp_result)
+    cv2.imwrite(f'ort_result_{opt.mode}.png', pp_result)
     
-    print(f"elpased time = {time_elpased:.3f}s")
+    print(f" [{opt.mode}] elpased time = {time_elpased:.3f}s")
