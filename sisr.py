bf07c1d240d2fe97db6b00b595799e19d61686cd
Longcat2957 <schrodingers030@gmail.com>
2023-03-02 14:50:32 +0900

    time

--- sisr.py
+++ sisr.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import onnx
 import onnxruntime as ort
@@ -11,9 +12,13 @@ if __name__ == '__main__':
     orig_img = load_image('ms3_01.png')
     test_input = preprocess(orig_img, (300, 300))
     
+    start = time.time()
     raw_result = session.run([], {input_name:test_input})[0]
+    time_elpased = time.time() - start
     
     pp_result = postprocess(raw_result)
     
     import cv2
     cv2.imwrite('ort_result_fp32.png', pp_result)
+    
+    print(f"elpased time = {time_elpased:.3f}s")
