c54ef9b5a27020f4858685bee13e0e1bc98b1516
Longcat2957 <schrodingers030@gmail.com>
2023-03-04 15:58:25 +0900

    minor change

--- sisr.py
+++ sisr.py
@@ -3,12 +3,15 @@ import time
 import numpy as np
 import onnx
 import onnxruntime as ort
-
+from tqdm import tqdm
 from data_utils import load_image, preprocess, postprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
     '--mode', type=str, default='int8', choices=['int8', 'fp32']
+)
+parser.add_argument(
+    '--rep', type=int, default=100
 )
 
 if __name__ == '__main__':
@@ -20,12 +23,16 @@ if __name__ == '__main__':
     test_input = preprocess(orig_img, (300, 300))
     
     start = time.time()
-    raw_result = session.run([], {input_name:test_input})[0]
+    for i in range(opt.rep):
+        raw_result = session.run([], {input_name:test_input})[0]
     time_elpased = time.time() - start
-    
+    print(f"# [{opt.mode}] total elpased time = {time_elpased:.2f}s")
+
+    time_elpased /= opt.rep
+    print(f"# [{opt.mode}] average elapsed time = {time_elpased:.5f}s")    
+
     pp_result = postprocess(raw_result)
     
     import cv2
     cv2.imwrite(f'ort_result_{opt.mode}.png', pp_result)
     
-    print(f" [{opt.mode}] elpased time = {time_elpased:.3f}s")
